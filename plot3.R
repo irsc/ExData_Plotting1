2d8908d6b158c2ee6687ef375a3b475a2aff9fc1
irsc <rivero.scastilla@gmail.com>
2014-10-12 15:49:09 +0200

    Second commit: plot3 added

---
+++ plot3.R
@@ -0,0 +1,26 @@
+# Set working directory
+#setwd("./Data_Science_Coursera/Scripts/4_ExpData")
+# Load data
+data<-read.table(file="household_power_consumption.txt",header=TRUE,sep=";")
+
+#Plot 3
+Sys.setlocale("LC_TIME", "English")
+# Open png device
+png("plot3.png", width = 480, height = 480)
+# Subset relevant dates
+data_01<-subset(data,Date=="1/2/2007")
+data_02<-subset(data,Date=="2/2/2007")
+# Merge subset datasets
+data2<-rbind(data_01,data_02)
+# Preparing data forplotting
+date<-paste(data2$Date,data2$Time)
+date2<-strptime(date,format="%d/%m/%Y %H:%M:%S")
+y1<-as.numeric(as.character(data2$Sub_metering_1))
+y2<-as.numeric(as.character(data2$Sub_metering_2))
+y3<-as.numeric(as.character(data2$Sub_metering_3))
+#Plot
+plot(date2,y1,ylab="Energy sub metering",xlab="",type="l",col="black")
+lines(date2,y2,ylab="Energy sub metering",xlab="",type="l",col="red")
+lines(date2,y3,ylab="Energy sub metering",xlab="",type="l",col="blue")
+legend("topright", lty=1,col = c("black", "red","blue"), legend = c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"))
+dev.off()
